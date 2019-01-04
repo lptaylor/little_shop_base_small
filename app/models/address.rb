3cb958cc2d441d1d7a0cae7ed111e1b7329c8a0e
Lance Taylor <lance.taylor98@hotmail.com>
2019-01-04 10:30:42 -0700

    adds controller and model for address. fixes validations for user

---
+++ app/models/address.rb
@@ -0,0 +1,4 @@
+class Address < ApplicationRecord
+  belongs_to :user
+  validates_presence_of :address, :city, :state, :zip
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   has_many :items, foreign_key: 'merchant_id'
   has_many :orders
   has_many :order_items, through: :orders
+  has_many :addresses
 
-  validates_presence_of :name, :address, :city, :state, :zip
+  validates_presence_of :name
   validates :email, presence: true, uniqueness: true
 
   enum role: [:default, :merchant, :admin]
