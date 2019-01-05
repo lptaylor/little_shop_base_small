7f75b3217114f9c80402ee60cb34e08f81da7125
Lance Taylor <lance.taylor98@hotmail.com>
2019-01-05 11:44:36 -0700

    adds testing and first link to profile to see add new address link

---
+++ spec/features/user/profile_addresses_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  before(:each) do
+    @user_1 = create(:user)
+    create(:address, user: @user_1, default_address: true)
+    create(:address, user: @user_1, default_address: false, enabled: false)
+    create(:address, user: @user_1, default_address: false)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+    visit profile_path(@user_1)
+  end
+
+  it 'allows user to add new addresses to existing addresses' do
+    click_link 'Add New Address', new_profile_addresses_path(@user_1)
+
+    nickname = "THIS IS A TEST"
+    address = "12345 Main St 2"
+    city = "Denver 2"
+    state = "CO 2"
+    zip = "80000 2"
+
+    fill_in :addresses_nickname , with: nickname
+    fill_in :addresses_address , with: address
+    fill_in :addresses_city , with: city
+    fill_in :addresses_state , with: state
+    fill_in :addresses_zip , with: zip
+
+    click_on 'Create Address'
+
+    expect(current_path).to eq(profile_path)
+    within ".address-#{addresses.last.id}" do
+      expect(page).to have_content(nickname)
+      expect(page).to have_content(address)
+      expect(page).to have_content(city)
+      expect(page).to have_content(state)
+      expect(page).to have_content(zip)
+    end
+  end
+end
